47cbf1ba738b2dd05000733ff865a5abfea8828c
Carlos Motta <chmottaa@gmail.com>
2025-11-02 21:08:10 -0300

    Criação de coletor
    Criação automatica de 3 coletores para teste.

---
+++ ecocoleta-api/src/main/java/br/com/ecocoleta/ecocoletaapi/configuracao/DadosColetores.java
@@ -0,0 +1,49 @@
+package br.com.ecocoleta.ecocoletaapi.configuracao;
+
+import br.com.ecocoleta.ecocoletaapi.entidades.UsuarioEntidade;
+import br.com.ecocoleta.ecocoletaapi.enums.UsuarioPerfil;
+import br.com.ecocoleta.ecocoletaapi.repositorio.UsuarioRepositorio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class DadosColetores implements ApplicationRunner {
+
+    private final UsuarioRepositorio usuarioRepositorio;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Iniciando cadastro coletores...");
+        String[][] listColetores = {
+                {"coletor1", "coletor1"},
+                {"coletor2", "coletor2"},
+                {"coletor3", "coletor3"}
+        };
+        for(String[] coletor : listColetores){
+            String nomeUsuario = coletor[0];
+            String senha = coletor[1];
+
+            if(usuarioRepositorio.findByNomeUsuario(nomeUsuario).isEmpty()){
+                UsuarioEntidade novoColetor = new UsuarioEntidade();
+                novoColetor.setNomeUsuario(nomeUsuario);
+                novoColetor.setSenha(senha);
+                novoColetor.setPerfil(UsuarioPerfil.COLETOR);
+                novoColetor.setComplemento("-");
+                novoColetor.setCep("-");
+                novoColetor.setLogradouro("-");
+                novoColetor.setCidade("-");
+                novoColetor.setEstado("-");
+                novoColetor.setBairro("-");
+                novoColetor.setNumero("-");
+
+                usuarioRepositorio.save(novoColetor);
+                System.out.println("Coletor "+nomeUsuario+" cadastrado para teste.");
+            }else {
+                System.out.println("Coletor "+nomeUsuario+" existente.");
+            }
+        }
+    }
+}
--- ecocoleta-api/src/main/java/br/com/ecocoleta/ecocoletaapi/repositorio/UsuarioRepositorio.java
+++ ecocoleta-api/src/main/java/br/com/ecocoleta/ecocoletaapi/repositorio/UsuarioRepositorio.java
@@ -1,7 +1,13 @@
 package br.com.ecocoleta.ecocoletaapi.repositorio;
 
+import br.com.ecocoleta.ecocoletaapi.dtos.UsuarioRespostaDto;
 import br.com.ecocoleta.ecocoletaapi.entidades.UsuarioEntidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepositorio extends JpaRepository<UsuarioEntidade, Long> {
+
+    Optional<UsuarioRespostaDto> findByNomeUsuario(String nomeUsuario);
+
 }
